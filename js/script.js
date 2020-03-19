a1be94b2a37f0cc52606a19d17b33283fd41f944
MahmoudProg <42582112+MahmoudProg@users.noreply.github.com>
2020-03-19 20:27:52 +0200

    Twast

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function ReadX(){
+      var x = document.getElementById("hdnSession").value;
+     alert(x);
+}
+function logout(){
+
+}
+function Users_Page(){
+      window.location.href = "Users.html";
+}
+function HomePage_Page(){
+      window.location.href = "HomePage.html";
+}
